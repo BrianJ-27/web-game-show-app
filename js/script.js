65e3cb6d6792415e0206f485bdafd5262ddb066b
Brian <johnson8727@hotmail.com>
2019-05-02 21:30:09 -0400

    still testing

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const qwerty = document.querySelector('qwerty');
-const phrase = document.querySelector('phrase');
-const missed = 0;
-const startButton = document.getElementsByClassName('btn_reset');
-
-startButton.addEventListener('click', () => {
-    startButton.style.display = 'none';
-})
