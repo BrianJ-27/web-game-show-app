45346abdca3949e07f1bb65b9ded572cb7ca6fdd
Brian <johnson8727@hotmail.com>
2019-05-03 17:13:21 -0400

    Added Specific Comments to app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const missed = 0;
 const qwerty = document.querySelector('qwerty');
 const phrase = document.querySelector('phrase');
 
+
+//** when a user clicks on the start button, the screen overlay is hidden to show gameboard 
 startGame.addEventListener('click', () => {
     overlay.style.display = 'none';
-});
+}); 
