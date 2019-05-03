738ab4508a7bf9656fc35bb3d625fce1338d3605
Brian <johnson8727@hotmail.com>
2019-05-02 21:22:54 -0400

    testing event listener

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const qwerty = document.querySelector('qwerty');
+const phrase = document.querySelector('phrase');
+const missed = 0;
+const startButton = document.getElementsByClassName('btn_reset');
+
+startButton.addEventListener('click', () => {
+    startButton.style.color = 'red';
+})
