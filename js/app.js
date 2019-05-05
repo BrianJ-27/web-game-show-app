05af5571a6691bf9219b4c60878af8a83c5ff2ed
Brian Johnson <johnson8727@hotmail.com>
2019-05-04 22:18:55 -0400

    deleted files

--- js/app.js
+++ js/app.js
@@ -3,13 +3,6 @@ const startGame = document.getElementsByClassName('btn__reset')[0];
 const missed = 0;
 const qwerty = document.querySelector('qwerty');
 const phrase = document.querySelector('phrase');
-
-
-//** when a user clicks on the start button, the screen overlay is hidden to show gameboard 
-startGame.addEventListener('click', () => {
-    overlay.style.display = 'none';
-}); 
-
 const gamePhrases = [
     'a dog walked to the park',
     'the boy ran outside the house',
@@ -18,9 +11,16 @@ const gamePhrases = [
     'The dog walked to the park'
 ];
 
+//** when a user clicks on the start button, the screen overlay is hidden to show gameboard 
+startGame.addEventListener('click', () => {
+    overlay.style.display = 'none';
+}); 
+
+
+
 const getRandomPhraseAsArray = arr => {
    let randomNmbr = Math.floor(Math.random() * 6);
-    return arr[randomNmbr];
+    return arr[randomNmbr]
    }
 
-console.log(getRandomPhraseAsArray());
+
