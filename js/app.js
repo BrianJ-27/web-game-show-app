1c2a017edd1758153d2bd4652f6f90cc464ca146
Brian Johnson <johnson8727@hotmail.com>
2019-05-08 07:06:22 -0400

    Added a couple more functions and const to my program

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ const startGame = document.getElementsByClassName('btn__reset')[0];
 const missed = 0;
 const qwerty = document.querySelector('qwerty');
 const phrase = document.querySelector('phrase');
-const gamePhrases = [
+const phraseArray = getRandomPhraseAsArray(phrases);
+const phrases = [
     'a dog walked to the park',
     'the boy ran outside the house',
     'Each man ran beyond the marker',
-    'four gotas ate grass today',
+    'four goats ate grass today',
     'The dog walked to the park'
 ];
 
@@ -16,11 +17,35 @@ startGame.addEventListener('click', () => {
     overlay.style.display = 'none';
 }); 
 
+// Note to self: character in the array is referring to the the arr[i] in my programming.. 
 
-
-const getRandomPhraseAsArray = arr => {
-   let randomNmbr = Math.floor(Math.random() * 6);
-    return arr[randomNmbr]
+/*--------------FUNCTIONS----------------*/ 
+//passing the "arr" variable in will make function reusable to pass any array into this function
+const getRandomPhraseAsArray = arr => { 
+// generating a random number from 1-5 & using that number to access the index of any array
+   let randomArray = arr[Math.floor(Math.random() * 5) +1]; 
+ // Return a new character array that split the strings inside the array into a new single line array  
+    return randomArray.split();  
    }
+// Passing the phrases array as in argument to the function 
+getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay(arr){
+    // do stuff any arr that is passed in, and add to `#phrase ul`
+    let phraseUl = document.querySelector('#phrase ul');
+    for(let i = 0; i < arr.length; i += 1) {
+      let li = document.createElement('li');
+      li.textContent = arr[i];
+      phraseUl.appendChild(li);
+      if (arr[i] == ' '){
+        li.classList.add('letter');
+      }else {
+        li.classList.add('space');
+      }
+  }
+}
+
+addPhraseToDisplay(phraseArray);
+
 
 
