753322f0bf8ace77c403e79f95fec856e261f5eb
Brian Johnson <johnson8727@hotmail.com>
2019-05-08 22:58:50 -0400

    corrected my addphrasestodisplay function so it works now & added comments to explain my work

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ const startGame = document.getElementsByClassName('btn__reset')[0];
 const missed = 0;
 const qwerty = document.querySelector('qwerty');
 const phrase = document.querySelector('phrase');
-const phraseArray = getRandomPhraseAsArray(phrases);
+
 const phrases = [
-    'a dog walked to the park',
-    'the boy ran outside the house',
-    'Each man ran beyond the marker',
-    'four goats ate grass today',
-    'The dog walked to the park'
+    'avengers forever',
+    'the black panther',
+    'tony stark ironman',
+    'the incredible hulk',
+    'in the endgame now'
 ];
 
 //** when a user clicks on the start button, the screen overlay is hidden to show gameboard 
@@ -25,27 +25,48 @@ const getRandomPhraseAsArray = arr => {
 // generating a random number from 1-5 & using that number to access the index of any array
    let randomArray = arr[Math.floor(Math.random() * 5) +1]; 
  // Return a new character array that split the strings inside the array into a new single line array  
-    return randomArray.split();  
+    return randomArray.split('');  
    }
-// Passing the phrases array as in argument to the function 
-getRandomPhraseAsArray(phrases);
 
-function addPhraseToDisplay(arr){
-    // do stuff any arr that is passed in, and add to `#phrase ul`
+// Passing the phrases array as in argument to the function 
+const singleArray = getRandomPhraseAsArray(phrases);
+
+const addPhraseToDisplay = arr =>{
+// get ul element from document and store in the variable phraseUl within this arrow expression
     let phraseUl = document.querySelector('#phrase ul');
-    for(let i = 0; i < arr.length; i += 1) {
-      let li = document.createElement('li');
-      li.textContent = arr[i];
-      phraseUl.appendChild(li);
-      if (arr[i] == ' '){
-        li.classList.add('letter');
+/* loop through an array and execute the following tasks: 
+    1) create a list element for each array character
+    2) each array character/letter & space is being assigned or placed into each newly created li element
+    3) we then append/ add the newly created li element to the DOM 
+    4) we test to see if the content with the li element is not equal to a space. If true then we add the className "letter" to the li element and its accompanying styles
+       if it test to false then we apply the className "space" to the li element along with its accompanying styles 
+*/     
+    for (let i = 0; i < arr.length; i += 1) {
+      let letter = document.createElement('li'); // Step 1
+      letter.innerText = arr[i]; // Step 2
+      phraseUl.appendChild(letter); // Step 3
+      if (letter.innerHTML !== ' ' ){ // Step 4 
+        letter.className = 'letter';
       }else {
-        li.classList.add('space');
+        letter.className = 'space';
       }
   }
 }
+// call the function to add the phrase array to the game screen
+addPhraseToDisplay(singleArray);
 
-addPhraseToDisplay(phraseArray);
 
+const checkLetter = buttonPicked => {
+  let letters = document.querySelectorAll('.letter');
+  for (let i = 0; i < letters.length; i += 1){
+    if(letters[i] === buttonPicked.innerText){
+      letters[i].className = 'show';
+    }else{
+      letters[i].className = null;
+    }
+  }
+}
+
+checkLetter();
 
 
