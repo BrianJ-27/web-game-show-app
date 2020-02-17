fd66b5e3e4a20b5fb269079a096f1cbc4d0be104
Brian Johnson <johnson8727@hotmail.com>
2020-02-16 23:01:27 -0500

    changed programming style to OOP ES6

--- js/app.js
+++ js/app.js
@@ -1,121 +1,116 @@
-/*-----------------------------------------------------------------------------------------------------*/
-//                                        **VARIABLES**                                                //
-/*-----------------------------------------------------------------------------------------------------*/
-
-const startGame = document.querySelector(".btn__reset");
-const showClass = document.getElementsByClassName("show");
-const qwerty = document.querySelector("#qwerty");
-const startTitle = document.querySelector(".title");
-const overlay = document.querySelector("#overlay");
-const letters = document.getElementsByClassName("letter");
-let phrase = document.querySelector("#phrase ul");
+/*=-=-=-=-=-GLOBAL VARIABLES & CLASS FOR ALL VARIABLES =-=-=-=-=-*/
 let missed = 0;
-const phrases = [
-  "avengers forever",
-  "the black panther",
-  "tony stark ironman",
-  "the incredible hulk",
-  "in the endgame now", 
-  "the amazing spiderman",
-  "thors hammer",
-  "the mighty mjolnir"
-];
+class Variables {
+  constructor(){
+    this.startGame = document.querySelector(".btn__reset");
+    this.showClass = document.getElementsByClassName("show");
+    this.qwerty = document.querySelector("#qwerty");
+    this.startTitle = document.querySelector(".title");
+    this.overlay = document.querySelector("#overlay");
+    this.letters = document.getElementsByClassName("letter");
+    this.phrase = document.querySelector("#phrase ul");
+    this.phrases = [
+      "avengers forever",
+      "the black panther",
+      "tony stark ironman",
+      "the incredible hulk",
+      "in the endgame now", 
+      "the amazing spiderman",
+      "thors hammer",
+      "the mighty mjolnir"
+    ];
+  }
+}
 
-/*-----------------------------------------------------------------------------------------------------*/
-//                                        **EVENT LISTENERS**                                      //
-/*-----------------------------------------------------------------------------------------------------*/
+/*=-=-=-=-=-GAME UI FUNCTION/METHODS=-=-=-=-=-*/
+class UI {
+  getRandomPhraseAsArray (arr){
+    let randomArray = arr[Math.floor(Math.random() * arr.length)];
+    let randomPhrase = randomArray.split("");
+    return randomPhrase;
+  }
 
-//** when a user clicks on the start button, the screen overlay is hidden to show gameboard
-startGame.addEventListener("click", () => {
-  reset();
-  overlay.style.display = "none";
-  const singleArray = getRandomPhraseAsArray(phrases);
-  addPhraseToDisplay(singleArray);
+   addPhraseToDisplay (arr){
+    for (let i = 0; i < arr.length; i += 1) {
+      const variable = new Variables();
+      let letter = document.createElement("li");
+      letter.innerText = arr[i];
+      variable.phrase.appendChild(letter);
+      letter.innerHTML !== " " ? letter.className = "letter" : letter.className = "space";
+    }
+  }
+
+  checkLetter (buttonPicked) {
+    const variable = new Variables();
+    let btn_match = false;
+    for (let i = 0; i < variable.letters.length; i += 1) {
+      let myLetter = variable.letters[i].textContent.toUpperCase();
+      let myButton = buttonPicked.textContent.toUpperCase();
+      if (myLetter === myButton) {
+        variable.letters[i].className += " show";
+        btn_match = true;
+      }
+    }
+    return btn_match;
+  }
+
+  checkWin () {
+    const variable = new Variables();
+    if (variable.letters.length === variable.showClass.length) {
+      variable.overlay.style.display = "flex";
+      variable.overlay.className = "win";
+      variable.startGame.textContent = "Another Round?";
+      variable.startTitle.textContent = "You Won!";
+      variable.startTitle.style.marginBottom = "0em";
+    } else if (missed >= 5) {
+      variable.overlay.style.display = "flex";
+      variable.overlay.className = "lose";
+      variable.startGame.textContent = "Try Again?";
+      variable.startTitle.textContent = "Game Over";
+      variable.startTitle.style.marginBottom = "0em";
+    }
+  }
+
+  reset () {
+    const variable = new Variables();
+    missed = 0;
+    let tries = document.querySelectorAll(".tries");
+    tries.forEach(element => element.className = "tries");
+  
+    let oldPhraseLetters = document.querySelectorAll("#phrase ul > li");
+    oldPhraseLetters.forEach(resetLetter => variable.phrase.removeChild(resetLetter));
+  
+    let keyboardKeys = document.querySelectorAll(".chosen");
+    keyboardKeys.forEach(resetKeys => {
+      resetKeys.className = resetKeys.className.replace("chosen", "");
+      resetKeys.disabled = false;
+    });
+  }
+}
+
+/*=-=-=-=-=-EVENT LISTENERS=-=-=-=-=-*/
+document.querySelector(".btn__reset").addEventListener("click", () => {
+  const ui = new UI();
+  const variable = new Variables();
+  ui.reset();
+  variable.overlay.style.display = "none";
+  const addRandomPhrase = ui.getRandomPhraseAsArray(variable.phrases);
+  ui.addPhraseToDisplay(addRandomPhrase);
 });
 
-qwerty.addEventListener("click", e => {
+document.querySelector("#qwerty").addEventListener('click', (e) =>{
+  const ui = new UI();
   let correctButton = e.target;
   if (correctButton.tagName === "BUTTON") {
     correctButton.className = "chosen";
     correctButton.disabled = true;
-    const letterFound = checkLetter(correctButton);
-    if (letterFound === false) {
+    const letterFound = ui.checkLetter(correctButton);
+    if (letterFound === false) { 
       let tries = document.querySelectorAll(".tries");
       let toHide = tries[missed];
       toHide.className += " hidden";
       missed += 1;
     }
-  }
-  checkWin();
+    ui.checkWin();
+  } 
 });
-
-/*-----------------------------------------------------------------------------------------------------*/
-//                                        **FUNCTIONS**                                                //
-/*-----------------------------------------------------------------------------------------------------*/
-
-// Resets the game when button is clicked **This function is called inside the start game listener
-const reset = () => {
-  missed = 0;
-  let tries = document.querySelectorAll(".tries");
-  tries.forEach(element => element.className = "tries");
-
-  let oldPhraseLetters = document.querySelectorAll("#phrase ul > li");
-  oldPhraseLetters.forEach(resetLetter => phrase.removeChild(resetLetter));
-
-  let keyboardKeys = document.querySelectorAll(".chosen");
-  keyboardKeys.forEach(resetKeys => {
-    resetKeys.className = resetKeys.className.replace("chosen", "");
-    resetKeys.disabled = false;
-  });
-};
-
-//passing the "arr" variable in will make function reusable to pass any array into this function
-const getRandomPhraseAsArray = arr => {
-  let randomArray = arr[Math.floor(Math.random() * phrases.length)];
-  let randomPhrase = randomArray.split("");
-  return randomPhrase; // ["i","n", "", "t",]
-};
-
-// function to add random phras to game board
-const addPhraseToDisplay = arr => {
-  for (let i = 0; i < arr.length; i += 1) {
-    let letter = document.createElement("li");
-    letter.innerText = arr[i];
-    phrase.appendChild(letter);
-    letter.innerHTML !== " " ? letter.className = "letter" : letter.className = "space";
-  }
-};
-
-//** This function will check if the button picked matches the letter in the phrase
-const checkLetter = buttonPicked => {
-  let btn_match = false;
-  for (let i = 0; i < letters.length; i += 1) {
-    let myLetter = letters[i].textContent.toUpperCase();
-    let myButton = buttonPicked.textContent.toUpperCase();
-    if (myLetter === myButton) {
-      letters[i].className += " show";
-      btn_match = true;
-    }
-  }
-  return btn_match;
-};
-
-//** the checkWin function will also be called inside the event listener
-const checkWin = () => {
-  if (letters.length === showClass.length) {
-    overlay.style.display = "flex";
-    overlay.className = "win";
-    startGame.textContent = "Another Round?";
-    startTitle.textContent = "You Won!";
-    startTitle.style.marginBottom = "0em";
-  } else if (missed >= 5) {
-    overlay.style.display = "flex";
-    overlay.className = "lose";
-    startGame.textContent = "Try Again?";
-    startTitle.textContent = "Game Over";
-    startTitle.style.marginBottom = "0em";
-  }
-};
-
-
-
